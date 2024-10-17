4d1a18970900ea0fb6c8082e3cfb42bf5434a7a6
Jerome Guterl <guterlj@fusion.gat.com>
2024-10-17 11:25:32 -0700

    fix example and init.py

--- gmsh_automated_scripts/example/DiMES_mesh_generation.py
+++ gmsh_automated_scripts/example/DiMES_mesh_generation.py
@@ -5,41 +5,28 @@ Created on Tue Oct 15 15:54:25 2024
 
 @author: cappellil
 """
-#%%
+# %%
 
-# set working directory equal to gmsh_automated_scripts directory
-import sys
-import os
+from gmsh_automated_scripts import generate_dimes_mesh
 
-# Add the path to the script directory
-script_path = os.path.abspath(os.environ['PWD'])
-sys.path.append("../")
-
-from gmsh_automated_scripts.utils2 import generate_dimes_mesh 
-# Initialize GMSH
-
-
-#%% 
 
 """ INPUT dots in structure """
 
-Names = ["Dot_1","Dot_2","Dot_3"]
-
 input_dict = {
-    Names[0]: {
+    "Dot_1": {
         "shape": "circle",
         "x": 0,
         "y": 0.75,
         "radius": 0.1  # For cylinder
     },
-    Names[1]: {
+    "Rectangle_2": {
         "shape": "rectangle",
         "x": -0.5,
         "y": -0.25,
         "width": 1,
         "height": 0.5
     },
-    Names[2]: {
+    "Dot_3": {
         "shape": "circle",
         "x": 0,
         "y": -0.75,
